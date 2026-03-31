56507ee6fc284f5763ee0501684095d1a432ee46
Gabriel <gabrielriferreira@hotmail.com>
2026-03-31 15:34:47 -0300

    Inserindo serializers e testes

--- bookstore/tests.py
+++ bookstore/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .serializers import UserSerializer
+from .models import User
 
-# Create your tests here.
+class UserModelTest(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create(
+            name="Gabriel",
+            email="gabriel@email.com"
+        )
+
+        self.assertEqual(user.name, "Gabriel")
+        self.assertEqual(user.email, "gabriel@email.com")
+
+
+
+class UserSerializerTest(TestCase):
+
+    def test_serializer_valid(self):
+        data = {
+            "name": "Gabriel",
+            "email": "gabriel@email.com"
+        }
+
+        serializer = UserSerializer(data=data)
+
+        self.assertTrue(serializer.is_valid())
