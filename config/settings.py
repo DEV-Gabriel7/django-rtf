d80697710b4f6f158b3516a67133783b90f4829d
Gabriel <gabrielriferreira@hotmail.com>
2026-04-29 16:01:39 -0300

    fix: remove auth no CI

--- config/settings.py
+++ config/settings.py
@@ -108,10 +108,17 @@ STATIC_URL = 'static/'
 
 # Django Rest Framework
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
+if os.environ.get('CI') == 'true':
+    REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.AllowAny',
+        ]
+    }
+else:
+    REST_FRAMEWORK = {
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework.authentication.TokenAuthentication',
+        ],
+    }
