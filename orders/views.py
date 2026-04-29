66d404077f73f160545bff258aa826b103b09bb0
Gabriel <gabrielriferreira@hotmail.com>
2026-04-29 16:10:06 -0300

    'INSERINDO'

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Order, User
@@ -8,10 +8,10 @@ from .serializers import OrderSerializer, UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all().order_by('id')
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
